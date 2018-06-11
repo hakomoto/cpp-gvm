983f8606a0badcf778f578ffcece07fbcaf02a46
Hakomoto <antosha.hakomoto@gmail.com>
2018-06-11 12:14:59 +0400

    Added header

--- h/Utils/MemInject.h
+++ h/Utils/MemInject.h
@@ -1,4 +1,25 @@
 #pragma once
+/*
+This file is part of cpp-gvm.
+
+cpp-gvm is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+cpp-gvm is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with cpp-gvm.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/** @file MemInject.h
+* @author Hakomoto <antosha.hakomoto@gmail.com>
+* @date 2018
+*/
+
 void CTrevalState::ResizeAllocator(unsigned int size)
 {
 	m_injector.Resize(size);
